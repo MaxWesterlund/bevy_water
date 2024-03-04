68007dcf504a3abe0d1271a5faf18a92ce26a9db
MaxWesterlund <max.westerlund3@gmail.com>
2024-03-04 21:18:48 +0100

    water added

---
+++ assets/shaders/custom_material_2d.wgsl
@@ -0,0 +1,59 @@
+#import bevy_sprite::mesh2d_vertex_output::VertexOutput
+
+@group(2) @binding(0) var<uniform> light_color: vec4<f32>;
+@group(2) @binding(1) var<uniform> medium_light_color: vec4<f32>;
+@group(2) @binding(2) var<uniform> medium_dark_color: vec4<f32>;
+@group(2) @binding(3) var<uniform> dark_color: vec4<f32>;
+@group(2) @binding(4) var<uniform> time: f32;
+
+
+@fragment
+fn fragment(
+    mesh: VertexOutput,
+) -> @location(0) vec4<f32> {
+    let resolution = 128.0;
+
+    let uv = mesh.uv;
+
+    let ratio = 1.0 / resolution;
+    let coord = vec2<f32>(
+        ratio * floor(uv.x / ratio),
+        ratio * floor(uv.y / ratio)
+    );
+
+    let wave_amount = 15;
+    let wave_speed = 0.07;
+
+    var amplitude = 1.0;
+    var frequency = 10.0;
+    
+    var val = 0.0;
+    var last_wave_derivative = 0.0;
+
+    for (var i = 0; i < wave_amount; i++) {
+        let angle = 3.5765 * f32(i);
+        let direction = vec2<f32>(cos(angle), sin(angle));
+
+        let content = (coord.x * direction.x + coord.y * direction.y) * frequency + time * wave_speed * frequency;
+
+        val += amplitude * sin(content);
+
+        last_wave_derivative = cos(content);
+
+        amplitude *= 0.82;
+        frequency *= 1.18;
+    }
+
+    if val < -1.5 {
+        return dark_color;
+    }
+    else if val < 0 {
+        return medium_dark_color;
+    }
+    else if val < 1.7 {
+        return medium_light_color;
+    }
+    else {
+        return light_color;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,96 @@
+//! A shader and a material that uses it.
+
+use bevy::{
+    prelude::*,
+    reflect::TypePath,
+    render::render_resource::{AsBindGroup, ShaderRef},
+    render::camera::ScalingMode,
+    sprite::{Material2d, Material2dPlugin, MaterialMesh2dBundle},
+};
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(
+            Color::hex("d0baa9").unwrap()
+        ))
+        .add_plugins((
+            DefaultPlugins,
+            Material2dPlugin::<CustomMaterial>::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, update_material_time)
+        .run();
+}
+
+// Setup a simple 2d scene
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<CustomMaterial>>,
+) {
+    // camera
+    commands.spawn(Camera2dBundle {
+        transform: Transform {
+            translation: Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: 10.0
+            },
+            ..default()
+        },
+        projection: OrthographicProjection {
+            scaling_mode: ScalingMode::AutoMin {
+                min_width: 512.0, 
+                min_height: 288.0
+            },
+            ..default()
+        },
+        ..default()
+    });
+
+    // quad
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Rectangle::default()).into(),
+        transform: Transform::default().with_scale(Vec3::splat(256.0)),
+        material: materials.add(CustomMaterial {
+            light_color: Color::hex("b9d4b4").unwrap(),
+            medium_light_color: Color::hex("6aae9d").unwrap(),
+            medium_dark_color: Color::hex("355d69").unwrap(),
+            dark_color: Color::hex("37364e").unwrap(),
+            time: 0.0,
+        }),
+        ..default()
+    });
+}
+
+// This is the struct that will be passed to your shader
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct CustomMaterial {
+    #[uniform(0)]
+    light_color: Color,
+    #[uniform(1)]
+    medium_light_color: Color,
+    #[uniform(2)]
+    medium_dark_color: Color,
+    #[uniform(3)]
+    dark_color: Color,
+    #[uniform(4)]
+    time: f32,
+}
+
+/// The Material2d trait is very configurable, but comes with sensible defaults for all methods.
+/// You only need to implement functions for features that need non-default behavior. See the Material2d api docs for details!
+impl Material2d for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        "shaders/custom_material_2d.wgsl".into()
+    }
+}
+
+fn update_material_time(
+    mut materials: ResMut<Assets<CustomMaterial>>,
+    time: Res<Time>,
+) {
+    for material in materials.iter_mut() {
+        material.1.time = time.elapsed_seconds();
+    }
+}
